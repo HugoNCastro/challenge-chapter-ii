a51c520d7668d0933a48514e2c2c1a4e8904daf6
Hugo Naaman Castro <hugonaaman@gmail.com>
2021-06-04 14:29:33 -0300

    All tests succeeded

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -1,15 +1,24 @@
 import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
-interface IRequest {
+/***interface IRequest {
   user_id: string;
-}
+}***/
 
 class ListAllUsersUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+  constructor(private usersRepository: IUsersRepository) { }
 
-  execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+  execute({ user_id }): User[] {
+    const userIsAdmin = this.usersRepository.findById(user_id);
+    if(!userIsAdmin){
+      throw new Error("User cannot be found !")
+    }
+    if (userIsAdmin.admin) {
+      const users = this.usersRepository.list();
+      return users;
+    } else {
+      throw new Error("Cannot list all users, your user is not admin!")
+    }
   }
 }
 
--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
@@ -1,3 +1,4 @@
+import { UsersRepository } from "../../repositories/implementations/UsersRepository";
 import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
@@ -9,8 +10,15 @@ class TurnUserAdminUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User {
-    // Complete aqui
+    const usersRepository = UsersRepository.getInstance();
+    const findUser = usersRepository.findById(user_id);
+    if(!findUser){
+      throw new Error("This user cannot be admin!");
+    }
+
+    const turnAdmin = usersRepository.turnAdmin(findUser);
+
+    return turnAdmin;
   }
 }
-
 export { TurnUserAdminUseCase };
